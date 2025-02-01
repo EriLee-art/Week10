3d3f96d54ae0f0870fea7823467e08ec4d759742
Eri Lee <ericallenlee2002@gmail.com>
2025-02-01 02:09:18 -0800

    Laying out what I'm going to do.

---
+++ scripts.js
@@ -0,0 +1,34 @@
+/**
+ * Week 10 Coding Assignment
+ * 
+ * ----------------------------------------------------------
+ * 
+ * Using HTML, Bootstrap, and JavaScript create a single page website that contains the following:
+ * A Bootstrap styled table representing your choice of data.
+ * A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
+ * 
+ */
+
+/**
+ *      Need to create a Table
+ * 
+ *  1. Create Columns <th>
+ *  2. Create Rows <tr>
+ *  3. Input information into the rows and columns <td>
+ *  4. Append them to the table
+ *  5. Functionality to remove objects from the table
+ * 
+ *      Does the table need the functionality to both create and delete data?
+ *  */ 
+
+
+
+
+
+/**
+ *      Need to create a Form
+ * 
+ *  1. When user clicks submit, add a new row to the table.
+ *  2. Form will create a new table row with new Table Data to coincide with it.
+ *  3. Append to the table.
+ */
